9077732b6ee0e88e75aea76672e0c0216b4af8b0
Easy.D <sempronx86@gmail.com>
2016-11-08 23:12:03 +0800

    添加两个按钮，新建账本和新建客户，同时修改UserLogin.java里面的onClick

--- app/src/main/java/com/rm/easy/crm/UserLogin.java
+++ app/src/main/java/com/rm/easy/crm/UserLogin.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TableRow;
 import android.widget.Toast;
 import com.rm.easy.crm.bean.JsonGeneral;
 import com.rm.easy.crm.iface.HttpCallbackListener;
@@ -22,6 +23,10 @@ public class UserLogin extends Activity implements View.OnClickListener {
 
     public static final int LOGIN_SUCCESS = 0;
     public static final int LOGIN_FAIL = 1;
+    public static final int CLICK_CLIENT_MANAGE = 2;
+    public static final int CLICK_FINANCING_MANAGE = 3;
+
+    Message message = new Message();
 
     private EditText userName;
     private EditText userPwd;
@@ -31,6 +36,12 @@ public class UserLogin extends Activity implements View.OnClickListener {
     private Button clientManage;
     private Button financingManage;
     private Button sendMsg;
+    private Button creatClient;
+    private Button creatFinancing;
+
+
+    private TableRow clientTableRow;
+    private TableRow financingTableRow;
 
 
     @Override
@@ -39,12 +50,25 @@ public class UserLogin extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_user_login);
         userName = (EditText) findViewById(R.id.uname);
         userPwd = (EditText) findViewById(R.id.upass);
+
+        clientTableRow = (TableRow) findViewById(R.id.client_tablerow);
+        financingTableRow = (TableRow) findViewById(R.id.financing_tablerow);
+
         orderManage = (Button) findViewById(R.id.order);
         warehouseManage = (Button) findViewById(R.id.warehouse);
         clientManage = (Button) findViewById(R.id.client);
         financingManage = (Button) findViewById(R.id.financing);
         sendMsg = (Button) findViewById(R.id.submit);
+        creatClient = (Button) findViewById(R.id.client_new);
+        creatFinancing = (Button) findViewById(R.id.financing_new);
+        orderManage.setOnClickListener(this);
+        warehouseManage.setOnClickListener(this);
+        clientManage.setOnClickListener(this);
+        financingManage.setOnClickListener(this);
         sendMsg.setOnClickListener(this);
+        creatClient.setOnClickListener(this);
+        creatFinancing.setOnClickListener(this);
+
     }
 
 
@@ -64,6 +88,7 @@ public class UserLogin extends Activity implements View.OnClickListener {
                 case LOGIN_FAIL:
                     String failRes = (String) msg.obj;
                     Toast.makeText(UserLogin.this, failRes, Toast.LENGTH_SHORT).show();
+                    break;
             }
         }
 
@@ -71,11 +96,28 @@ public class UserLogin extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
-        if(view.getId() == R.id.submit){
-            String address = "http://192.168.31.218/www/user/select_user.php";
-            String reqStr = "uname=" + userName.getText() + "&upwd=" + userPwd.getText();
-            sendRequest(address, reqStr);
+        switch (view.getId()){
+
+            case R.id.submit:
+                Log.i("UserLogin", "Click Submit");
+                String address = "http://192.168.31.218/www/user/select_user.php";
+                String reqStr = "uname=" + userName.getText() + "&upwd=" + userPwd.getText();
+                sendRequest(address, reqStr);
+                break;
+            case R.id.client:
+                Log.i("UserLogin", "Click Client Manage");
+                financingTableRow.setVisibility(View.GONE);
+                clientTableRow.setVisibility(VISIBLE);
+                break;
+            case R.id.financing:
+                Log.i("UserLogin", "Click Financing Manage");
+                clientTableRow.setVisibility(View.GONE);
+                financingTableRow.setVisibility(VISIBLE);
+                break;
+
+
         }
+
     }
 
     private void sendRequest(String address, String reqStr){
@@ -84,7 +126,7 @@ public class UserLogin extends Activity implements View.OnClickListener {
             public void onFinish(String response) {
 
                 JsonGeneral jsonGenerals = new GsonUtil().parseJsonWithGson(response, JsonGeneral.class);
-                Log.i("jsonGenerals", jsonGenerals.getStatus());
+                Log.i("UserLogin", jsonGenerals.getStatus());
                 Message msg = new Message();
                 //此处判断服务器的响应，成功返回Success，失败返回Fail
                 if(jsonGenerals.getStatus().equals("Success")) {
