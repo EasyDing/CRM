f6d1e360139776617caa67f8d085c757fa2a7760
EasyOffice <sempronx86@gmail.com>
2016-11-11 17:03:36 +0800

    完成网络连通性判断

--- app/src/main/java/com/rm/easy/crm/UserLogin.java
+++ app/src/main/java/com/rm/easy/crm/UserLogin.java
@@ -90,7 +90,7 @@ public class UserLogin extends Activity implements View.OnClickListener {
                     break;
                 case LOGIN_FAIL:
                     String failRes = (String) msg.obj;
-                    //判断网络链接情况，无返回表示网络不联通，进行重新握手；联通以后进行判断
+                    //判断网络链接情况，无返回表示网络不联通，进行重新握手；联通以后进行判断.
                     if(failRes.equals("")){
                         Toast.makeText(UserLogin.this, "建立连接中，请稍候", Toast.LENGTH_SHORT).show();
                         Timer timer = new Timer();
